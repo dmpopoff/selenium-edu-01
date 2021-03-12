72c1b44bf28aaa81219bd5dc69c05d5efd0b39ed
Попов Дмитрий Викторович <d.popov@faberlic.com>
2021-03-12 13:43:13 +0300

    add comments. clean up code

--- litecart_adm_menuitems.py
+++ litecart_adm_menuitems.py
@@ -7,28 +7,24 @@ time.sleep(2)
 for itemno in range(len(arr1)):
     print("меню верхнего уровня номер: "+str(itemno+1))
     xpath = "(//li[@id='app-'])["+str(itemno+1)+"]/a"
-    # print(xpath)
-    time.sleep(0.5)
+    time.sleep(0.5) # Chrome bug: cannot click to first menu entry. Alexey Barancev
     driver.find_element_by_xpath(xpath).click()
     arr2 = driver.find_element_by_xpath("(//li[@id='app-'])["+str(itemno+1)+"]").find_elements_by_css_selector("li")
     print("кол-во подменю: "+str(len(arr2)))
     headerH1 = driver.find_elements_by_css_selector("h1")
     if len(headerH1)>0:
-        pass #print("H1 is here")
+        print("H1 is here")
     else:
         print("H1 is not here")
     for submenu in range(len(arr2)):
-        # k = 23/0
         print("     подменю номер: "+str(submenu+1))
-        time.sleep(0.5)
+        time.sleep(0.5) # Chrome bug: cannot click to first menu entry. Alexey Barancev
         topmenu = driver.find_element_by_xpath("(//li[@id='app-'])["+str(itemno+1)+"]")
         submenu_item = topmenu.find_element_by_xpath("(.//li)["+str(submenu+1)+"]")
         submenu_item.click()
         headerH1 = driver.find_elements_by_css_selector("h1")
         if len(headerH1)>0:
-            pass #print("     H1 is here")
+            print("     H1 is here")
         else:
             print("     H1 is not here")
-        # driver.find_element_by_xpath("((//li[@id='app-'])["+str(itemno+1)+"]//li)["+str(submenu+1)+"]").click()
-    # print(arr2)
 driver.quit()
