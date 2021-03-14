4c2c5227ea8a2a9e0a775c702ed8a9eb78431c36
Попов Дмитрий Викторович <d.popov@faberlic.com>
2021-03-14 23:43:03 +0300

    task8 done

--- task8/task8_stickers.py
+++ task8/task8_stickers.py
@@ -6,4 +6,9 @@ driver = webdriver.Chrome(options=options)
 driver.get("http://localhost/litecart/en/")
 
 product_carts = driver.find_elements_by_css_selector("li.product")
-print(len(product_carts))
+for sticker in range(len(product_carts)):
+    if len(product_carts[sticker].find_elements_by_css_selector("div.sticker"))>0:
+        print("sticker is here!")
+    else:
+        print("where is no sticker here(((")
+driver.quit()
